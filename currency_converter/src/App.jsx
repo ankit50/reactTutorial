8aaca282693acb36317f0c53f21e307132723b33
Ankit Kharel <ankeetkharel@gmail.com>
2025-03-31 22:39:15 +0545

    component create complete

--- currency_converter/src/App.jsx
+++ currency_converter/src/App.jsx
@@ -1,9 +1,15 @@
 
 function App() {
+  
   return (
-    <>
-      <h1>Ankit Kharel</h1>
-    </>
+    <div 
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,}}>\
+      <div className="">
+
+      </div>
+
+    </div>
   )
 }
 
